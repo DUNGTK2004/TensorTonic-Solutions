67266d6c6f2c94b495b97b3182ec13f5ab5b0f00
DungTK2004 <117809086+DUNGTK2004@users.noreply.github.com>
2026-03-25 11:23:31 +0700

    Update solution for angle-between-3d

--- angle-between-3d/angle-between-3d.py
+++ angle-between-3d/angle-between-3d.py
@@ -5,11 +5,17 @@ def angle_between_3d(v, w):
     Compute the angle (in radians) between two 3D vectors.
     """
     # Your code here
-    dot = np.dot(v, w)
+    v = np.array(v)
+    w = np.array(w)
+
+    eps = 1e-10
+
     norm_v = np.linalg.norm(v)
     norm_w = np.linalg.norm(w)
-    if norm_v == 0 or norm_w == 0:
+    if norm_v < eps or norm_w < eps:
         return np.nan
+        
+    dot = np.dot(v, w)
     cos = dot / (norm_v * norm_w)
-    theta = np.arccos(cos)
-    return theta
+    
+    return np.arccos(np.clip(cos, -1.0, 1.0))
